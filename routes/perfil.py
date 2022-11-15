e024b4a6df3b77ac2e96f211c8f9795fb04cc724
MiguelGuillen3 <116836017+MiguelGuillen3@users.noreply.github.com>
2022-11-15 13:39:01 -0500

    Update perfil.py

--- routes/perfil.py
+++ routes/perfil.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from bottle import Bottle, template, HTTPResponse
+from daos.perfil_dao import get_all
 from configs.helpers import menu
 
 subapp = Bottle()
@@ -11,6 +12,7 @@ def home():
   locals = {
     'title': 'Perfil',
     'menu': menu('/perfil'),
+    'perfil': get_all(1),
   }
   boby_template = template('perfil/index', locals = locals)
   return HTTPResponse(status = 200, body = boby_template)

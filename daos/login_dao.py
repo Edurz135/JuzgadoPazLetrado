112f878b77887ef359e24e9465b3d34b594a2471
IMacFlyI <116754369+IMacFlyI@users.noreply.github.com>
2022-11-20 13:13:33 -0500

    prueba olvidar contraseña

--- daos/login_dao.py
+++ daos/login_dao.py
@@ -6,3 +6,11 @@ def get_juez(user, pswd):
   SELECT * FROM JUEZ WHERE USUARIO = '{user}' AND CONTRASENA = '{pswd}'
   """
   return [dict(r) for r in conn.execute(stmt)]
+
+def get_password(user):
+  conn = engine.connect()
+  stmt = f"""
+  SELECT JUEZ.CONTRASENA FROM JUEZ WHERE USUARIO = '{user}' 
+  """
+  return [dict(r) for r in conn.execute(stmt)]
+
--- routes/inicio.py
+++ routes/inicio.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bottle import Bottle, template, HTTPResponse
+from bottle import Bottle, template, HTTPResponse, request
+from daos.login_dao import get_password
 from configs.helpers import menu
 
 subapp = Bottle()
@@ -11,6 +12,15 @@ def home():
   locals = {
     'title': 'Inicio',
     'menu': menu('/inicio'),
+  }
+  boby_template = template('inicio/index', locals = locals)
+  return HTTPResponse(status = 200, body = boby_template)
+
+@subapp.route('/olvidar', method='GET')
+def home():
+  user = request.params.juez_usuario
+  locals = {
+    'contrasena' : get_password(user)
   }
   boby_template = template('inicio/index', locals = locals)
   return HTTPResponse(status = 200, body = boby_template)

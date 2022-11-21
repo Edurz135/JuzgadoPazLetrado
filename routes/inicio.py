77bd8ac0f4d0263db80a8dbf767893c32b551299
RBrunoChV <edu.rz@hotmail.com>
2022-11-20 22:28:39 -0500

    pantalla inicio

--- routes/inicio.py
+++ routes/inicio.py
@@ -2,16 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from bottle import Bottle, template, HTTPResponse, request
-from daos.login_dao import get_password
+from daos.perfil_dao import get_by_id
 from configs.helpers import menu
 
 subapp = Bottle()
 
 @subapp.route('/', method='GET')
 def home():
+  juez_id = request.params.juez_id or 1
   locals = {
     'title': 'Inicio',
     'menu': menu('/inicio'),
+    'perfil': get_by_id(juez_id),
   }
+  print(locals['perfil'])
   boby_template = template('inicio/index', locals = locals)
   return HTTPResponse(status = 200, body = boby_template)
--- views/inicio/index.tpl
+++ views/inicio/index.tpl
@@ -1,6 +1,6 @@
 % include('_header.tpl')
 <div style="display: flex; flex-direction: column; height: 70vh; align-items: center; justify-content: center">
-  <h1 style="font-size: 50px">Bienvenido</h1>
+  <h1 style="font-size: 50px">Bienvenido {{locals['perfil'][0]['juez_nombres']}} {{locals['perfil'][0]['juez_apellidos']}}</h1>
   <p>
     En esta plataforma tendrá todas las herramientas necesarias para ver la
     información correspondiente a las demandas que le han sido asignadas
